5dc8e9a6a3e7ade37d7e7c07ee99e43ecfac0205
estevaolibardi <estevaolibardi@hotmail.com>
2023-07-28 17:16:30 -0300

    feat: routes and layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/theme/default';
 import { GlobalStyle } from './styles/global';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Routes';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <ThemeProvider theme={defaultTheme}></ThemeProvider>
-    </>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
